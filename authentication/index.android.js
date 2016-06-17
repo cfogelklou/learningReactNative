367756a2555fc68b628b324843e542e6f1cf9897
cfogelklou <chris.fogelklou@gmail.com>
2016-06-17 08:48:59 +0200

    Runs on both ios and android.

--- authentication/index.android.js
+++ authentication/index.android.js
@@ -3,7 +3,7 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
+/*
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -11,44 +11,14 @@ import {
   Text,
   View
 } from 'react-native';
-
+*/
 //ssh chris@192.168.163.129
 
-class authentication extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+'use strict';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+var React = require('react-native');
+var {AppRegistry} = React;
 
-AppRegistry.registerComponent('authentication', () => authentication);
+var Main = require('./src/main');
+
+AppRegistry.registerComponent('authentication', () => Main);
